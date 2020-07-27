df8d995c41db73f061480b855e9bd6c2a2807ec6
vhamilton08 <67924236+vhamilton08@users.noreply.github.com>
2020-07-27 16:49:27 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const getProducts = require("./getProducts")
+const getProduct = require("./getProduct")
+const app = express()
+
+const port = 3001
+
+app.get("/api/products", getProducts)
+app.get("/api/product/:id", getProduct)
+
+app.listen(port, () => {
+    console.log(`Server listening on ${port}`)
+})
+
